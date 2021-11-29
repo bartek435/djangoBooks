d387ea7d2adc9084fd225f016fe8f3a885f262ed
bartek <bartekmichta@gmail.com>
2021-11-29 15:57:55 +0100

    debug false

--- djangoBooks/settings.py
+++ djangoBooks/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
